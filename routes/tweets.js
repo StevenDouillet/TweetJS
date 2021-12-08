45cbbf6633d0b00f8d169af0f06204e5820ab6e3
DOUILLET <Steven.douillet@gmail.com>
2021-12-08 14:15:47 +0100

    create tweet, add test datas and manage errors

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,10 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
-router.post('/tweets', (req, res) => {
-  const body = req.body;
-  console.log(body);
-  res.send(body);
+let tweets = JSON.parse(fs.readFileSync('./public/json/tweets.json'));
+
+router.get('/', (req, res, next) => {
+  res.render('tweets', { tweets });
+});
+
+router.get('/new', (req, res) => {
+  res.render('newTweet', {});
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+
+  const tweet = tweets.find((elem) => {
+    return elem.id === id;
+  });
+
+  if(tweet === undefined || tweet == null) {
+    const error = {
+      'status': '404',
+      'stack': 'tweet-not-found'
+    }
+    res.render('error', { error });
+  } else {
+    res.render('tweet', { tweet });
+  }
+});
+
+router.post('/', (req, res) => {
+  const tweet = req.body;
+  tweet.id = uuidv4();
+  tweets.push(tweet);
+  res.redirect('/tweets');
 });
 
 module.exports = router;
